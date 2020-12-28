317e961323a88261fbdbbacd6dfab756a429e4d8
marira <rongrazzo@gmail.com>
2020-12-29 02:36:31 +0600

    Mobile styling

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className={`App ${libraryStatus ? "library-active" : ""}`}>
       <Nav
         libraryStatus={libraryStatus}
         setLibraryStatus={setLibraryStatus}
